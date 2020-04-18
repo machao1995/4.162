c02f89e9e8eb6a6ba9a755c619ff32349b677e57
caonima19951202 <1598214715@qq.com>
2020-04-18 13:37:35 +0800

    fghjk
    Signed-off-by: caonima19951202 <1598214715@qq.com>

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import  forms
 class Person_Info(forms.Form):
     name = forms.CharField(max_length=64,)  # 字符串类型，最长不超过4个字符
+
     birthday = forms.CharField(max_length=64)         #日期类型
     email = forms.CharField(max_length=64)  # required=False表示可以为空，默认是True，不能为空
     phone = forms.CharField(max_length=64)       #数值类型
--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,13 @@ from . import models
 
 
 def index(request):
-    blog=Blog.objects.all().first()
+    Blog = models.Blog()
+    blog=models.Blog.objects.all().first()
     blog_content=markdown.markdown(blog.content)
     return render(request,"blog.html",{'blog':blog,"blog_content":blog_content})
 def persondetail(request):
-    blog=Blog.objects.all().first()
+    Blog = models.Blog()
+    blog=models.Blog.objects.all().first()
     # blog_content=markdown.markdown(blog.content)
     return render(request,"persondetail.html",{'blog':blog,"blog_content":
 
@@ -23,11 +25,64 @@ from .forms import Person_Info  #导入form表单
 
 
 
+# def person(request):
+#
+#     person_obj = Person_Info()  # 创建了这个对象
+#     if request.method == 'POST':
+#         form_obj = Person_Info(request.POST)
+#         if form_obj.is_valid():
+#             print("form is valid ^_^** ", form_obj.cleaned_data)  # 返回表单数据，是字典形式
+#
+#
+#
+#             name = form_obj.cleaned_data.get('name')  # 取出填写的name的值
+#             Blog =models.Blog()
+#
+#             Blog.name=form_obj.cleaned_data.get('name')
+#             Blog.birthday = form_obj.cleaned_data.get('birthday')
+#             Blog.email = form_obj.cleaned_data.get('email')
+#             Blog.phone = form_obj.cleaned_data.get('phone')
+#             # Blog.name = name
+#
+#
+#             Blog.is_active = 0
+#             Blog.save()
+#
+#
+#             # if name == 'lily':
+#             #     return HttpResponse('*lily*')
+#         else:
+#             errors = form_obj.errors
+#             print("form is invalid T_T... ", errors)  # .errors 获取错误信息
+#
+#             return render(request, 'errorinfo.html', {'obj': form_obj, 'errors': form_obj.errors})
+#
+#     return render(request,'info.html',{'obj':person_obj})  #然后把对象传给html
+
+from .models import Article_Model_Form
+def article_modelform(request):
+    # form = Article_Model_Form()  #创建一个表单来添加一条数据
+    if request.method == 'POST':
+        form = Article_Model_Form(request.POST)
+        if form.is_valid():   # 判断表单是否合法
+            print("form is ok", form.cleaned_data)
+            form.save()   # 将合法的数据保存到数据表中
+        else:
+            error_msg = form.errors
+            # return render(request, 'blog.html', {"article_form": form ,"errors": error_msg})
+    # return render(request, 'blog.html', {"article_form": form})
+
+
+
+
 def person(request):
 
     person_obj = Person_Info()  # 创建了这个对象
+    data = models.Blog.objects.all()
     if request.method == 'POST':
         form_obj = Person_Info(request.POST)
+
+
         if form_obj.is_valid():
             print("form is valid ^_^** ", form_obj.cleaned_data)  # 返回表单数据，是字典形式
 
@@ -53,21 +108,11 @@ def person(request):
             errors = form_obj.errors
             print("form is invalid T_T... ", errors)  # .errors 获取错误信息
 
-            return render(request, 'errorinfo.html', {'obj': form_obj, 'errors': form_obj.errors})
+            return render(request, 'errorinfo.html', {'obj': form_obj,'errors': form_obj.errors})
 
-    return render(request,'info.html',{'obj':person_obj})  #然后把对象传给html
+    return render(request,'info.html',{'obj':person_obj, 'data':data})  #然后把对象传给html
 
 from .models import Article_Model_Form
-def article_modelform(request):
-    # form = Article_Model_Form()  #创建一个表单来添加一条数据
-    if request.method == 'POST':
-        form = Article_Model_Form(request.POST)
-        if form.is_valid():   # 判断表单是否合法
-            print("form is ok", form.cleaned_data)
-            form.save()   # 将合法的数据保存到数据表中
-        else:
-            error_msg = form.errors
-            # return render(request, 'blog.html', {"article_form": form ,"errors": error_msg})
-    # return render(request, 'blog.html', {"article_form": form})
+
 
 
